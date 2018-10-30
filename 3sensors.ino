e316acc2a88a78b902a7364eed3fe940304a78a1
NikS8 <nik.8@i.ua>
2018-10-30 21:53:56 +0300

    add ArduinoJson

--- 3sensors.ino
+++ 3sensors.ino
@@ -1,3 +1,24 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\
+                                                    3sensors.ino 
+                                         Copyright © 2018, Nik.S 
+    30.10.2018 v0.1
+\*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*******************************************************************\
+   Сервер ArduinoJson выдает значения: 
+          аналоговый: 
+              от датчика дыма и газа MQ9
+          цифровые: 
+              от датчика дыма и газа MQ9
+              от датчика движения HC-SR501
+              от датчика температуры и вдажности DHT22
+/*******************************************************************/
+
+#include <ArduinoJson.h>
+#include <Ethernet2.h>
+#include <SPI.h>
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+EthernetServer server(80);
+
 #include <DHT.h>        // You have to download DHT22  library
 #define DHT1PIND 7       // PIN подключения датчика DTH22
 #define DHT1TYPE DHT22 
@@ -13,14 +34,27 @@ int sensorMQd;  //  цифровой сигнал датчика дыма
 #define HCSRPIND 5 //  цифровойPIN подключения датчика движения
 int sensorSRd;  //  цифровой сигнал датчика движения
 
-
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\
+\*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 void setup() {
   Serial.begin(9600);
+// Initialize Ethernet libary
+  if (!Ethernet.begin(mac)) {
+    Serial.println(F("Failed to initialize Ethernet library"));
+    return;
+  }
+// Start to listen
+  server.begin();
+  Serial.println(F("Server is ready."));
+  Serial.print(F("Please connect to http://"));
+  Serial.println(Ethernet.localIP());
+
   pinMode(MQ1PINA, INPUT);
   dht1.begin();
 
 }
-
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\
+\*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 void loop() {
   sensorDhtHum = dht1.readHumidity();
   sensorDhtTemp = dht1.readTemperature();
@@ -29,4 +63,53 @@ void loop() {
   sensorMQd = digitalRead(MQ1PIND);
 
   sensorSRd = digitalRead(HCSRPIND);
+
+  // Wait for an incomming connection
+  EthernetClient client = server.available();
+
+  // Do we have a client?
+  if (!client) return;
+
+  Serial.println(F("New client"));
+
+  // Read the request (we ignore the content in this example)
+  while (client.available()) client.read();
+
+  // Allocate JsonBuffer
+  // Use arduinojson.org/assistant to compute the capacity.
+  StaticJsonBuffer<500> jsonBuffer;
+
+  // Create the root object
+  JsonObject& root = jsonBuffer.createObject();
+
+  // Create the "analog" array
+  JsonArray& analogValues = root.createNestedArray("analog");
+ /* for (int pin = 0; pin < 6; pin++) {
+    // Read the analog input
+    int value = analogRead(pin);
+
+    // Add the value at the end of the array
+    analogValues.add(value);
+*/
+    analogValues.add(sensorMQa);
+  }
+  // Create the "digital" array
+  JsonArray& digitalValues = root.createNestedArray("digital");
+/*  for (int pin = 0; pin < 14; pin++) {
+    // Read the digital input
+    int value = digitalRead(pin);
+
+    // Add the value at the end of the array
+    digitalValues.add(value);
+  */
+    digitalValues.add(sensorMQd);
+    digitalValues.add(sensorSRd);
+    digitalValues.add(sensorDhtHum);
+    digitalValues.add(sensorDhtTemp);
+  
+  }
+
 }
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\
+\*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
