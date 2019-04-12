aae146bd8ed6a32742e899932cf4fa7251dbf18c
NikS8 <33399815+NikS8@users.noreply.github.com>
2019-04-12 23:02:49 +0300

    Update 3sensors.ino
    Add SR501

--- 3sensors.ino
+++ 3sensors.ino
@@ -5,13 +5,28 @@ DHT dht1(DHT1PIN, DHT1TYPE);
 int sensorDhtTemp;      // температура от датчика DHT22 в прихожей 
 int sensorDhtHum;       // влажность от датчика DHT22
 
+//  Управление светодиодной лентой датчиками движения SR501
+#define PIN_PIR1 2 // назначаем порт для PIR1 sensor
+#define PIN_RELE 13 // назначаем порт для реле
+unsigned long counttime; // выделение памяти для счетчика
+
 void setup() {
   Serial.begin(9600);
   dht1.begin();
+  pinMode(PIN_RELE, OUTPUT);
+  pinMode(PIN_PIR1, INPUT);
 }
 
 void loop() {
     sensorDhtHum = dht1.readHumidity();
     sensorDhtTemp = dht1.readTemperature();
+if (digitalRead(PIN_PIR1) == HIGH) { 
+        counttime = millis();
+    }
 
+    if (millis() - counttime > 20000) {
+        digitalWrite(PIN_RELE, LOW);
+    } else {
+        digitalWrite(PIN_RELE, HIGH);
+}
 }
